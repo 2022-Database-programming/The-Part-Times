3860a379d70b55d4b71e1b1803499395def60251
sylee <sy>
2022-11-27 15:55:01 +0900

    feat: EmployerWorkplaceDao remove 작성

--- src/main/java/model/dao/EmployerWorkplaceDao.java
+++ src/main/java/model/dao/EmployerWorkplaceDao.java
@@ -53,4 +53,20 @@ public class EmployerWorkplaceDao {
         return 0;
     }
 
+    public int remove(int memberId, int workplaceId) {
+        String sql = "delete from EMPLOYER_WORKPLACE " +
+                "where MEMBER_ID=1 and WORKPLACE_ID=1235";
+        jdbcUtil.setSqlAndParameters(sql, new Object[] { memberId, workplaceId});
+
+        try {
+            return jdbcUtil.executeUpdate();
+        } catch (Exception e) {
+            jdbcUtil.rollback();
+            e.printStackTrace();
+        } finally {
+            jdbcUtil.commit();
+            jdbcUtil.close();
+        }
+        return 0;
+    }
 }

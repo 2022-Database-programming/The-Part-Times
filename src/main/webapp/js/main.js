23687a18fdd803e21efc6b5e8a4548852f167812
sylee <sy>
2022-12-22 03:42:13 +0900

    feat: #36 ajax 템플릿 추가

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -15,16 +15,32 @@ angular
             defaultDate: todayDate,
             minDate: "2022-01-01",
             maxDate: "2023-12-31",
-            disabledDates: [
-
-            ],
+            disabledDates: [],
             dayNamesLength: 1, // 1 for "M", 2 for "Mo", 3 for "Mon"; 9 will show full day names. Default is 1.
             mondayIsFirstDay: true,//set monday as first day of week. Default is false
             eventClick: function(date) {
-                console.log(date);
+                console.log('event ', date);
             },
             dateClick: function(date) {
-                console.log(date);
+                console.log('date ', date);
+
+                // var params = {
+                //     today      : todayDate
+                // }
+                //
+                // // ajax 통신
+                // $.ajax({
+                //     type : "GET",            // HTTP method type(GET, POST) 형식이다.
+                //     url : "/test/ajax",      // 컨트롤러에서 대기중인 URL 주소이다.
+                //     data : params,            // Json 형식의 데이터이다.
+                //     success : function(res){ // 비동기통신의 성공일경우 success콜백으로 들어옵니다. 'res'는 응답받은 데이터이다.
+                //         // 응답코드 > 0000
+                //         alert(res.code);
+                //     },
+                //     error : function(XMLHttpRequest, textStatus, errorThrown){ // 비동기 통신이 실패할경우 error 콜백으로 들어옵니다.
+                //         alert("통신 실패.")
+                //     }
+                // });
             },
             changeMonth: function(month, year) {
                 console.log(month, year);
